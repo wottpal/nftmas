257214f5f52c6833e25282b79edea53689422640
Dennis Zoma <wottpal@users.noreply.github.com>
2022-01-09 17:51:24 +0100

    added temporary redirect until launch for production

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'ipfs.io', 'gateway.ipfs.io'],
   },
+  async redirects() {
+    return [
+      ...(process?.env?.NEXT_PUBLIC_PRODUCTION_MODE === 'true' ? [
+        {
+          source: '/',
+          destination: 'https://twitter.com/dennis_zoma',
+          permanent: false,
+        }
+      ] : []),
+    ]
+  },
 }
 
 module.exports = nextConfig
